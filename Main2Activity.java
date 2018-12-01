75cf4ff806e54f311b395bf2509ebbc7ad6e8e44
lawatkins <luisaw@bu.edu>
2018-12-01 16:05:25 -0500

    all the updated files for the project
    sadly, these files don't work with the emulator... but I figured I would continue with the design of each activity.

---
+++ Main2Activity.java
@@ -0,0 +1,76 @@
+package com.example.luisawatkins.mypetapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class Main2Activity extends Activity implements OnClickListener{
+
+    private static final String TAG_DEBUG = Main2Activity.class.getName();
+    public static final String TAG_NAME = "tag";
+
+    private EditText et;
+
+    private Button button02;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        et = (EditText) findViewByID(R.id.editText1);
+        button02 = (Button) findViewById(R.id.create);
+
+        button02.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        double pet = 0.0d;
+        switch(v.getId()){
+            case R.id.create :
+            {
+                pet = 1.1d;
+                break;
+            }
+            default :
+            {
+                break;
+            }
+        }
+
+
+        String name = et.getText().toString();
+
+        if(name.equals(""))
+        {
+            Toast.makeText(Main2Activity.this, "Oops! Make sure to give me a name.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        launchNextActivity(name);
+    }
+
+    private void launchNextActivity(String name)
+    {
+        double pet = getIntent().getExtras().getDouble(MainActivity.TAG_PET);
+
+        Intent nextActivity = new Intent(Main2Activity.this, Main3Activity.class);
+
+        nextActivity.putExtra(TAG_NAME,name);
+
+        String
+
+
+        startActivity(nextActivity);
+    }
+
+}
--- MainActivity.java
+++ MainActivity.java
@@ -1,18 +1,60 @@
-package com.example.luisawatkins.helloworld;
+package com.example.luisawatkins.mypetapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends Activity implements OnClickListener {
 
     private Button button01;
+
+    public static final String TAG_PET = "tag";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button01 = (Button) findViewById(R.id.play);
+
+        button01.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        double pet = 0.0d;
+
+
+        switch(v.getId()){
+            case R.id.play :
+            {
+                pet = 1.0d;
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+
+        launchNextActivity(pet);
+
+    }
+
+    private void launchNextActivity(double pet)
+    {
+
+        Intent NextActivity = new Intent(MainActivity.this, Main2Activity.class);
+
+        NextActivity.putExtra(TAG_PET, pet);
+
+        startActivity(NextActivity);
+    }
+
 }
